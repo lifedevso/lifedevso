77dc74e89994b1ead29b7fa44c7acb47080a0575
SengMitnick <sengmitnick@163.com>
2024-11-17 00:11:26 +0800

    重构根布局组件，优化结构，调整产品和服务链接，添加新产品链接，保持版权信息一致性

--- app/[locale]/(root)/layout.tsx
+++ app/[locale]/(root)/layout.tsx
@@ -4,79 +4,89 @@ import { RootHeader } from "@/components/RootHeader";
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
-    <html lang="zh-cn">
-      <body>
-        <RootHeader shouldShowSignInButton />
-        {children}
-        <footer className="bg-background border-t">
-          <div className="max-w-7xl mx-auto px-6 py-8">
-            <div className="flex flex-wrap">
-              <div className="w-full md:w-1/3 text-center md:text-left px-8">
-                <p className="uppercase mb-6 font-bold">人生发展体系</p>
-                <div className="flex flex-col">
-                  以「产」为核心的人生发展体系。
-                  <br />
-                  个人发展，一年顶十年。
-                </div>
-              </div>
-              <div className="w-full md:w-1/3 text-center md:text-left px-8">
-                <p className="uppercase mb-6 font-bold">产品</p>
-                <ul className="mb-4">
-                  <li className="mt-2">
-                    <Link
-                      href="https://notion-nice.com/"
-                      target="_blank"
-                      className="hover:underline text-gray-600 hover:text-gray-800"
-                    >
-                      Notion Nice 一键排版
-                    </Link>
-                  </li>
-                  <li className="mt-2">
-                    <Link
-                      href="https://harvest.superai42.com/"
-                      target="_blank"
-                      className="hover:underline text-gray-600 hover:text-gray-800"
-                    >
-                      Harvest 收藏助手
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-
-              <div className="w-full md:w-1/3 text-center md:text-left px-8">
-                <p className="uppercase mb-6 font-bold">服务</p>
-                <ul className="mb-4">
-                  <li className="mt-2">
-                    <Link
-                      target="_blank"
-                      href="https://lifedevso.notion.site/13e6b503659380738c37dfbec8574ee9"
-                      className="hover:underline text-gray-600 hover:text-gray-800"
-                    >
-                      合约条款及细则
-                    </Link>
-                  </li>
-                </ul>
+    <>
+      <RootHeader shouldShowSignInButton />
+      {children}
+      <footer className="bg-background border-t">
+        <div className="max-w-7xl mx-auto px-6 py-8">
+          <div className="flex flex-wrap">
+            <div className="w-full md:w-1/3 text-center md:text-left px-8">
+              <p className="uppercase mb-6 font-bold">人生发展体系</p>
+              <div className="flex flex-col">
+                以「产」为核心的人生发展体系。
+                <br />
+                个人发展，一年顶十年。
               </div>
             </div>
-            <div className="mt-4 space-y-2 text-xs text-gray-400">
-              <p>Copyright © 2024 人生发展体系. All Rights Reserved.</p>
-              <p>香港知行教育科技有限公司</p>
-              <p className="flex gap-1 items-center">
-                深圳知行合一教育科技有限公司
-                <span>|</span>
-                <a
-                  href="https://beian.miit.gov.cn/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="cursor-pointer"
-                >
-                  粤ICP备2024292309号-2
-                </a>
-              </p>
+            <div className="w-full md:w-1/3 text-center md:text-left px-8">
+              <p className="uppercase mb-6 font-bold">产品</p>
+              <ul className="mb-4">
+                <li>
+                  <Link
+                    href="https://harvest.superai42.com/"
+                    target="_blank"
+                    className="hover:underline"
+                    prefetch={false}
+                  >
+                    Harvest 收藏助手
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://notion-nice.com/"
+                    target="_blank"
+                    className="hover:underline"
+                    prefetch={false}
+                  >
+                    Notion Nice 一键排版
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://www.shining.so/"
+                    target="_blank"
+                    className="hover:underline"
+                    prefetch={false}
+                  >
+                    Shining 妙绘灵光
+                  </Link>
+                </li>
+              </ul>
+            </div>
+
+            <div className="w-full md:w-1/3 text-center md:text-left px-8">
+              <p className="uppercase mb-6 font-bold">服务</p>
+              <ul className="mb-4">
+                <li className="mt-2">
+                  <Link
+                    target="_blank"
+                    href="https://lifedevso.notion.site/13e6b503659380738c37dfbec8574ee9"
+                    className="hover:underline text-gray-600 hover:text-gray-800"
+                  >
+                    合约条款及细则
+                  </Link>
+                </li>
+              </ul>
             </div>
           </div>
-        </footer>
-      </body>
-    </html>
+          <div className="mt-4 space-y-2 text-xs text-gray-400">
+            <p>Copyright © 2024 人生发展体系. All Rights Reserved.</p>
+            <p>香港知行教育科技有限公司</p>
+            <p className="flex gap-1 items-center">
+              深圳知行合一教育科技有限公司
+              <span>|</span>
+              <a
+                href="https://beian.miit.gov.cn/"
+                target="_blank"
+                rel="noreferrer"
+                className="cursor-pointer"
+              >
+                粤ICP备2024292309号-2
+              </a>
+            </p>
+          </div>
+        </div>
+      </footer>
+    </>
   );
 }
