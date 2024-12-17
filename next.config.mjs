6810136bcf5c3c39381d2c5893e9a6da6c176a87
SengMitnick <sengmitnick@163.com>
2024-12-17 23:35:41 +0800

    简化ESLint配置，移除不必要的插件和扩展，更新Next.js配置以支持独立输出

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,10 @@
 import createNextIntlPlugin from "next-intl/plugin";
-import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
-
-// Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
-// (when running the application with `next dev`), for more information see:
-// https://github.com/cloudflare/next-on-pages/blob/main/internal-packages/next-dev/README.md
-if (process.env.NODE_ENV === "development") {
-  await setupDevPlatform();
-}
 
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   redirects: async () => {
     return [
       {
